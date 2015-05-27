6dbb227506127c6eea0156afa240dc9fb13ff937
wangxs <xiaoshuwang@creditease.cn>
2015-05-27 14:55:54 +0800

    multi machine communication,configure files,  code of logistic regression on 20131215

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-logistic-regression-v0.1
-========================
-
-logistic regression c++ code,SGD algorithm,read one line from the file,and then process it,then calculate,then updata theta.then get next line.it is very slowly.
-   
-    how to use it:
-        1, make 
-        2, ./lr "your train_file"
-    you can change the predict file in the code,and make it again.
+LogisticRegression-MPI-batch-sgd-v0.4
+=====================================
+    I will push a shell script soon: run.sh.
+    and just run: sh run.sh.
+    you should install MPI.
--- lr.h
+++ lr.h
@@ -22,12 +22,13 @@ class LR{
 	vec sample_feature_vec;
         vec_vec feature_matrix;
         vector<int> label;
-	void load_one_sample(string sample_file);  
+	void loadmodel(vector<float> &theta);  
         float sigmoid(float x);
         vector<string> splitline(string &line); 
 	void init_theta(vector<float>& theta, vector<float>& delta_theta,int feature_size);
+        void getfeature(string line, vector<int>& label,vec_vec& feature_matrix,int myid,int numprocs);
+        void train(string sample_filename, vector<float>& theta, vector<float> &delta_theta,vector<int>& label, vec_vec& feature_matrix,int myid,int numprocs); 
         int get_feature_num(string sample_file, vector<int>& label,vec_vec& feature_matrix,int myid,int numprocs);
-        void train(string filename,vector<float>& theta, vector<float> &delta_theta,vector<int>& label, vec_vec& feature_matrix,int myid,int numprocs);
         void savemodel(vector<float> &theta, int myid);
         void predict(string filename, vector<float>& theta);
     private:
