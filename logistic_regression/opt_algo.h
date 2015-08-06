b99d3a4bb46b7c8417a811b86020a5b6fff1025a
wangxs <xiaoshuwang@creditease.cn>
2015-08-06 08:32:42 +0800

    define gradient and w float* type

--- logistic_regression/opt_algo.h
+++ logistic_regression/opt_algo.h
@@ -22,34 +22,41 @@ public:
     ~OPT_ALGO();
 
     void load_data(std::string train_data_file, std::string split_tag);
-    std::vector<std::string> split_line(std::string line, std::string split_tag);
+    std::vector<std::string> split_line(std::string split_tag);
     void cal_fea_dim();
     void init_theta();
     void owlqn(int proc_id, int n_procs);
-    void parallel_owlqn(float *local_theta); 
-    void f_grad(float *local_theta, float *g);
+    void parallel_owlqn(); 
+    void f_grad(float *para_w, float *para_g);
     float sigmoid(float x);
-    void sub_gradient(float *g, float *sub_g);
-    void two_loop(float *local_theta, float *sub_g, float **s_list, float **y_list, float *ro_list);
-    void fix_dir(float *sub_g, float *g);
-    void line_search(float *sub_g, float *local_theta, float *next_theta);
-    float f_val(float *local_theta);
-
+    void sub_gradient(float *local_g, float *local_sub_g);
+    void two_loop(float *sub_g, float **s_list, float **y_list, float *ro_list, float *p);
+    void fix_dir(float *w, float *next_w);
+    void line_search(float *local_g);
+    float f_val(float *w);
+    //not shared by multithreads
+    std::string line;
+    std::vector<std::string> tmp_vec;
     std::string index_str;
     std::vector<std::string> feature_index;
     std::vector<sparse_feature> key_val;                                     
     sparse_feature sf;
-
+    //shared by multithreads
     std::vector<std::vector<sparse_feature> > fea_matrix;
     std::vector<float> label;
-    std::vector<float> theta;
+    float *w;
+    float *next_w;
+    float *global_g;
+    float *global_next_g;
     std::string train_file;
     std::string test_file;
     std::string split_tag;
-
     long int fea_dim;
     float c;
     int m;
+    int n_threads;
+    float global_old_loss_val;
+    float global_new_loss_val;
 
 private:
     
