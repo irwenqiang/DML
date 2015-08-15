0432f6082ed7531f8a86973fff7d5461cb19d06d
aromazyl <zyl2336709@gmail.com>
2015-08-15 22:08:44 +0800

    error correct

--- logistic_regression/main.cpp
+++ logistic_regression/main.cpp
@@ -1,10 +1,10 @@
-#include "opt_algo.h"
-#include "mpi.h"
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
 #include <iomanip>
 #include "gtest/gtest.h"
+#include "opt_algo.h"
+#include "mpi.h"
 
 struct ThreadParam{
     OPT_ALGO *opt;
@@ -13,7 +13,7 @@ struct ThreadParam{
     int n_process;
 };
 
-void *opt_algo(void *arg){
+void* opt_algo(void* arg){
    ThreadParam *args = (ThreadParam*)arg;
    args->opt->owlqn(args->process_id, args->n_process); 
 }
@@ -25,21 +25,18 @@ int main(int argc,char* argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     
-    std::string train_data_file = "./data/traindata.txt";
-    std::string test_data_file = "./data/testdata.txt";
+    std::string train_data_file("./data/traindata.txt");
+    std::string test_data_file("./data/testdata.txt");
     std::string split_tag = "\t";
 
-    //call by main thread
-    OPT_ALGO opt;
-    opt.load_data(train_data_file, split_tag);
-    opt.cal_fea_dim();
+    //call by main process
+    OPT_ALGO opt(train_data_file, split_tag);
     MPI_Bcast(&opt.fea_dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    opt.init_theta();
 
     //multithread start
     std::vector<ThreadParam> params;
     std::vector<pthread_t> threads;
-    for(int i = 0; i < opt.n_threads; i++){//construct parameter
+    for(int i = 0; i < opt.n_threads; i++) {//construct parameter
         ThreadParam param = {&opt, myid, numprocs};
         params.push_back(param);
     } 
@@ -50,7 +47,6 @@ int main(int argc,char* argv[]){
         int ret = pthread_create(&thread_id, NULL, &opt_algo, (void*)&(params[i])); 
         if(ret != 0) std::cout<<"process "<<i<<"failed(create thread faild.)"<<std::endl;
         else threads.push_back(thread_id);
-            
     }
     for(int i = 0; i < threads.size(); i++){//join threads function
         pthread_join(threads[i], 0); 
