a7bb40774c34e0476ff5a50783f54ea372aa8c56
aromazyl <zyl2336709@gmail.com>
2015-08-15 23:03:31 +0800

    make file change and folder added

--- logistic_regression/main.cpp
+++ logistic_regression/main.cpp
@@ -13,18 +13,19 @@ struct ThreadParam{
     int n_process;
 };
 
-void* opt_algo(void* arg){
+void *opt_algo(void* arg){
    ThreadParam *args = (ThreadParam*)arg;
    args->opt->owlqn(args->process_id, args->n_process); 
+   return NULL;
 }
 
-int main(int argc,char* argv[]){  
+int main(int argc,char* argv[]){
     int myid, numprocs;
-    MPI_Status status;
+    // MPI_Status status;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    
+
     std::string train_data_file("./data/traindata.txt");
     std::string test_data_file("./data/testdata.txt");
     std::string split_tag = "\t";
@@ -40,16 +41,16 @@ int main(int argc,char* argv[]){
         ThreadParam param = {&opt, myid, numprocs};
         params.push_back(param);
     } 
-    pid_t main_thread_id;
-    main_thread_id = getpid();
-    for(int i = 0; i < params.size(); i++){
+    // pid_t main_thread_id;
+    // main_thread_id = getpid();
+    for(size_t i = 0; i < params.size(); i++){
         pthread_t thread_id;
-        int ret = pthread_create(&thread_id, NULL, &opt_algo, (void*)&(params[i])); 
+        int ret = pthread_create(&thread_id, NULL, opt_algo, (void*)&(params[i])); 
         if(ret != 0) std::cout<<"process "<<i<<"failed(create thread faild.)"<<std::endl;
         else threads.push_back(thread_id);
     }
-    for(int i = 0; i < threads.size(); i++){//join threads function
-        pthread_join(threads[i], 0); 
+    for(size_t i = 0; i < threads.size(); i++){//join threads function
+        pthread_join(threads[i], 0);
     }
 
     MPI::Finalize();
